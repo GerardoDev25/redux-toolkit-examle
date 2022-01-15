51d9c1b382cb86a5d1ed78c468a525f7c10023f4
Gerardo Miranda <gerardodev25@gmail.com>
2022-01-14 22:15:06 -0400

    part-5-async-logic

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import { Provider } from 'react-redux';
 import './index.css';
 
 import App from './App';
+import { fetchUsers } from './features/users/usersSlice'
 import { store } from './app/store';
 import { worker } from './api/server';
 
 async function start() {
   await worker.start({ onUnhandledRequest: 'bypass' });
 
+  store.dispatch(fetchUsers())
+
+  
   ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
